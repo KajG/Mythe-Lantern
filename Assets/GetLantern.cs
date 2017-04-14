62ee3b6ce94a70fc6460f8f5e13d10d7efbaaf1c
KajG <kaaiman99@hotmail.com>
2017-04-14 09:42:13 +0200

    lantrnnn

---
+++ Assets/GetLantern.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetLantern : MonoBehaviour {
+
+	public bool hold;
+	public GameObject lantern;
+	public BoxCollider mouse;
+	public Vector3 mousePos;
+	private GameObject tempLantern;
+	public bool objDestroyed = true;
+	void Start () {
+		tempLantern = Instantiate (lantern, mousePos, Quaternion.identity);
+
+	}
+	
+	void Update () {
+		mousePos = Input.mousePosition;
+		mousePos.z = 10;
+		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+		mouse.transform.position = mousePos;
+		if (!objDestroyed) {
+			tempLantern.transform.position = mousePos;
+		}
+		if (Input.GetMouseButton (0)) {
+			hold = true;
+		} else {
+			hold = false;
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (!hold && other.gameObject.tag == "Mouse" && !objDestroyed) {
+			objDestroyed = true;
+			Destroy (tempLantern);
+		}
+	}
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.tag == "Mouse" && hold && objDestroyed) {
+			objDestroyed = false;
+			tempLantern = Instantiate (lantern, mousePos, Quaternion.identity);
+		}
+	}
+}
